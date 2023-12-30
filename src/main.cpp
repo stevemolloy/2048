6bbd5453bd83d250580a2b45edaea03de7cef7c2
Stephen Molloy <stephen.molloy@maxiv.lu.se>
2023-12-30 20:28:49 +0100

    Movement is implemented. But wrong.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <cmath>
+#include <cstdlib>
 #include <format>
 #include <iostream>
 #include <random>
@@ -52,6 +53,95 @@ public:
     }
   }
 
+  void MoveLeft() {
+    for (int i=GRIDSIZE-1; i>0; i--) {
+      for (int j=0; j<GRIDSIZE; j++) {
+        if (exponents[i][j] == 0) continue;
+        if (exponents[i-1][j] == 0) {
+          exponents[i-1][j] = exponents[i][j];
+          exponents[i][j] = 0;
+        } else if (exponents[i-1][j] == exponents[i][j]) {
+          exponents[i-1][j] += 1;
+          exponents[i][j] = 0;
+        }
+      }
+    }
+    int i1, j1;
+    getARandomSquare(&i1, &j1);
+    exponents[i1][j1] = 4;
+  }
+
+  void MoveRight() {
+    for (int i=0; i<GRIDSIZE-1; i++) {
+      for (int j=0; j<GRIDSIZE; j++) {
+        if (exponents[i][j] == 0) continue;
+        if (exponents[i+1][j] == 0) {
+          exponents[i+1][j] = exponents[i][j];
+          exponents[i][j] = 0;
+        } else if (exponents[i+1][j] == exponents[i][j]) {
+          exponents[i+1][j] += 1;
+          exponents[i][j] = 0;
+        }
+      }
+    }
+    int i1, j1;
+    getARandomSquare(&i1, &j1);
+    exponents[i1][j1] = 4;
+  }
+
+  void MoveUp() {
+    for (int i=0; i<GRIDSIZE; i++) {
+      for (int j=GRIDSIZE-1; j>0; j--) {
+        if (exponents[i][j] == 0) continue;
+        if (exponents[i][j-1] == 0) {
+          exponents[i][j-1] = exponents[i][j];
+          exponents[i][j] = 0;
+        } else if (exponents[i][j-1] == exponents[i][j]) {
+          exponents[i][j-1] += 1;
+          exponents[i][j] = 0;
+        }
+      }
+    }
+    int i1, j1;
+    getARandomSquare(&i1, &j1);
+    exponents[i1][j1] = 4;
+  }
+
+  void MoveDown() {
+    for (int i=0; i<GRIDSIZE; i++) {
+      for (int j=0; j<GRIDSIZE-1; j++) {
+        if (exponents[i][j] == 0) continue;
+        if (exponents[i][j+1] == 0) {
+          exponents[i][j+1] = exponents[i][j];
+          exponents[i][j] = 0;
+        } else if (exponents[i][j+1] == exponents[i][j]) {
+          exponents[i][j+1] += 1;
+          exponents[i][j] = 0;
+        }
+      }
+    }
+    int i1, j1;
+    getARandomSquare(&i1, &j1);
+    exponents[i1][j1] = 4;
+  }
+
+  int getARandomSquare(int *i1, int *j1) {
+    int empty_squares = CountEmptySquare();
+    if (empty_squares < 1) return -1;
+    int square1 = rand() % empty_squares;
+    int counter = 0;
+    for (size_t i=0; i<GRIDSIZE; i++) {
+      for (size_t j=0; j<GRIDSIZE; j++) {
+        if (square1 == counter) {
+          *i1 = i;
+          *j1 = j;
+        }
+        counter++;
+      }
+    }
+    return 1;
+  }
+
 private:
   int width, height, box_width, box_height;
   size_t exponents[GRIDSIZE][GRIDSIZE];
@@ -63,6 +153,16 @@ private:
 
   int Value(int i, int j) {
     return 1 << exponents[i][j];
+  }
+
+  int CountEmptySquare() {
+    size_t result = 0;
+    for (size_t i=0; i<GRIDSIZE; i++) {
+      for (size_t j=0; j<GRIDSIZE; j++) {
+        if (exponents[i][j] == 0) result++;
+      }
+    }
+    return result;
   }
 };
 
@@ -90,6 +190,10 @@ int main(void) {
   SetTargetFPS(60);
 
   while (!WindowShouldClose()) {
+    if (IsKeyPressed(KEY_LEFT))  grid.MoveLeft();
+    if (IsKeyPressed(KEY_RIGHT)) grid.MoveRight();
+    if (IsKeyPressed(KEY_UP))    grid.MoveUp();
+    if (IsKeyPressed(KEY_DOWN))  grid.MoveDown();
     BeginDrawing();
     ClearBackground(BACKGROUND_COLOUR);
     grid.Draw();
